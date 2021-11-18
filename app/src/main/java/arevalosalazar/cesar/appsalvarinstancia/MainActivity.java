a669a7173ca76314ddd52a3a1089af7cb8187f54
DAM2A-02 <dam2a-02@iesjovellanos.org>
2021-11-18 13:45:58 +0100

    Landscape

--- app/src/main/java/arevalosalazar/cesar/appsalvarinstancia/MainActivity.java
+++ app/src/main/java/arevalosalazar/cesar/appsalvarinstancia/MainActivity.java
@@ -7,31 +7,34 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int valor = 0;
+    int valor ;
     TextView tv;
     private final static String VALOR_GUARDADO = "valor";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = findViewById(R.id.tvNumero);
-        if(savedInstanceState == null){
-            valor=savedInstanceState.getInt(VALOR_GUARDADO);
+        if (savedInstanceState != null) {
+            valor = savedInstanceState.getInt(VALOR_GUARDADO);
             tv.setText(String.valueOf(valor));
-        }else tv.setText(R.string.numero);
+        } else {
+            tv.setText(R.string.numero);
+        }
     }
 
-    public void OnSaveInstanceState(Bundle estado)
-    {
+    protected void OnSaveInstanceState(Bundle estado) {
         super.onSaveInstanceState(estado);
-        estado.putInt(VALOR_GUARDADO,valor);
+        estado.putInt(VALOR_GUARDADO, valor);
     }
-    public void SumarUno(View v){
+
+    public void SumarUno(View v) {
         valor++;
         tv.setText(String.valueOf(valor));
     }
 
-    public void RestarUno(View v){
+    public void RestarUno(View v) {
         valor--;
         tv.setText(String.valueOf(valor));
     }
